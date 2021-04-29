3fd9e1ab4d232b694830743b152311b8d8511559
Yuren Ju <yurenju@gmail.com>
2021-04-29 15:52:24 +0800

    feat: add asset name for querying position

--- src/command/position.ts
+++ src/command/position.ts
@@ -1,13 +1,15 @@
+import AmmArtifact from "@perp/contract/build/contracts/src/Amm.sol/Amm.json"
 import ClearingHouseArtifact from "@perp/contract/build/contracts/src/ClearingHouse.sol/ClearingHouse.json"
 import chalk from "chalk"
-import { Contract } from "ethers"
+import { Contract, utils } from "ethers"
 import { CommandModule } from "yargs"
 import { toNumber } from "../util/casting"
 import { formatProperty } from "../util/format"
 import { fetchMetadata } from "../util/metadata"
 import { getProvider } from "../util/provider"
 import { getStageName } from "../util/stage"
-import { ClearingHouse } from "../type"
+import { Amm, ClearingHouse } from "../type"
+import { getContract } from "../util/contract"
 
 const DEFAULT_BLOCK_LIMIT = 10
 
@@ -45,6 +47,7 @@ const positionCommand: CommandModule = {
         const blockNumber = await provider.getBlockNumber()
         const trader = (argv["trader"] as string) || null
         const blockLimit = (argv["blockLimit"] as number) || DEFAULT_BLOCK_LIMIT
+        const ammPairMap = new Map<string, string>()
         const filter = clearingHouse.filters.PositionChanged(
             trader,
             null,
@@ -61,19 +64,31 @@ const positionCommand: CommandModule = {
             null,
         )
         const events = await clearingHouse.queryFilter(filter, blockNumber - blockLimit, blockNumber)
-        events.forEach((event, i) => {
+        for (let i = 0; i < events.length; i++) {
+            const event = events[i]
             const side = event.args.exchangedPositionSize.gt(0) ? "Buy" : "Sell"
             const positionNotional = toNumber(event.args.positionNotional)
             const exchangedPositionSize = toNumber(event.args.exchangedPositionSize)
             const price = Math.abs(positionNotional / exchangedPositionSize)
+            let pairName = ""
+
+            if (ammPairMap.has(event.args.amm)) {
+                pairName = ammPairMap.get(event.args.amm) || ""
+            } else {
+                const amm = getContract<Amm>(event.args.amm, AmmArtifact.abi, provider)
+                pairName = utils.parseBytes32String(await amm.priceFeedKey())
+                ammPairMap.set(event.args.amm, pairName)
+            }
+
             console.log(chalk.green(`PositionChanged event #${i + 1}`))
             console.log(formatProperty("trader", event.args.trader))
+            console.log(formatProperty("asset", pairName))
             console.log(formatProperty("side", side))
             console.log(formatProperty("price", price))
             console.log(formatProperty("size", exchangedPositionSize))
             console.log(formatProperty("tx", event.transactionHash))
             console.log("")
-        })
+        }
     },
 }
 
